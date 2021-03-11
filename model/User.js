088ea741e1e624850c3bc546808a715c590364cc
sumana2001 <sumana.basu2001@gmail.com>
2021-03-11 11:16:46 +0530

    Joi validation

--- model/User.js
+++ model/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        min:6,
         max:255
     },
     email:{
--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,38 @@
-const router = require('express').Router();
-const User = require('../model/User');
+const router = require("express").Router();
+const User = require("../model/User");
 
+//Validation
+const Joi = require("@hapi/joi");
 
-router.post('/register', async (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  });
-  try{
-    const savedUser= await user.save();
-    res.send(savedUser);
-  } catch(err){
-    res.status(400).send(err);
-  }
+const schema = Joi.object({
+  name: Joi.string()
+      .min(6)
+      .required(),
+  email: Joi.string()
+      .min(6)
+      .required()
+      .email(),
+  password: Joi.string()
+      .min(6)
+      .required()
+});
 
+router.post("/register", async (req, res) => {
+  //Validate User
+  const validation = schema.validate(req.body);
+  res.send(validation);
+
+  // const user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // });
+  // try {
+  //   const savedUser = await user.save();
+  //   res.send(savedUser);
+  // } catch (err) {
+  //   res.status(400).send(err);
+  // }
 });
 
 module.exports = router;
